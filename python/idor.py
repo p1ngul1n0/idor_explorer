7ecb73e3db35d9d9dc164d10bd5744e86bcd9ae2
sampzzz <57546870+sampzzz@users.noreply.github.com>
2020-07-18 22:22:28 -0300

    Add files via upload

--- python/idor.py
+++ python/idor.py
@@ -1,89 +1,117 @@
 import requests
 import time
 import warnings
-#Lib to explore IDOR in APIs
-warnings.simplefilter("ignore")
-class target: #TARGET CLASS
-	def __init__(self, url,): #target initilization
+from colorama import *
+import ast
+import regex
+from urllib.parse import urlparse
 
+init(autoreset=True)
+warnings.simplefilter("ignore")
+class target:
+	def __init__(self,url,method,payload_type,*args,**kwargs): #target initilization
 		print ("_"*100)
-		methods = ["GET","POST","PUT"]
-		self.method = methods[int(input("   > Select a method\n     >> 1-GET\n     >> 2-POST\n     >> 3-PUT\n     >> Insert ID:"))-1]
-		self.url = url #set object parameters
-		self.cookies = {}
+		self.url = url
+		self.method = method
+		self.payload_type = payload_type
 		self.headers = {}
 		self.proxies = {}
-		self.post_data = {}
 
-		if (input("   > Use cookies ? (y) ") == "y"): #ask for cookies
-			while(True): #cookies addition loop
-				cookie_name = input("     >> Enter new cookie name: ")
-				cookie_value = input("     >> Enter new cookie value: ")
-				self.cookies[cookie_name] = cookie_value
-				if(input("     >> Add more cookies ? (y) ") != "y"):
-					break
+		#Verify if optional arguments are set
+		if (kwargs.get('cookies') != None):
+			self.headers.update({"Cookie": kwargs.get('cookies')})
 
-		if (input("   > Use custom headers ? (y) ") == "y"): #ask for headers
-			while(True): #headers addition loop
-				header_name = input("     >> Enter new cookie name: ")
-				header_value = input("     >> Enter new cookie value: ")
-				self.headers[header_name] = header_value
-				if(input("     >> Add more headers ? (y) ") != "y"):
-					break
-		if (input("   > Use proxy ? (y) ") == "y"): #ask for proxy
-			proxy_host = input("     >> Enter proxy host: ")
-			proxy_port = input("     >> Enter proxy port: ")
-			self.proxies["http"] = "http://"+proxy_host+":"+proxy_port
-			self.proxies["https"] = "http://"+proxy_host+":"+proxy_port
+		if (kwargs.get('proxy') != None):
+			self.proxies["http"] = "http://"+kwargs.get('proxy')
+			self.proxies["https"] = "http://"+kwargs.get('proxy')
 
-		if (self.method == "POST" or self.method == "PUT"): #If request method post or put ask for body data
-			print (self.method)
-			self.post_data = input("   > Enter body data with IDOR on payload place: ")
-			encoding = int(input("     >> Select a encoding\n     >> 1-JSON\n     >> Insert ID:")) #ask for encoding
-			if (encoding == 1): #Add according Content Type
+		if (kwargs.get('headers') != None):
+			self.headers.update(ast.literal_eval(kwargs.get('headers')))
+
+		if (kwargs.get('post_data') != None):	
+			self.post_data = kwargs.get('post_data')
+			if (len(regex.compile(r'\{(?:[^{}]|(?R))*\}').findall(self.post_data)) >= 1):
 				self.headers['Content-Type'] =  'application/json'
+		else:
+			self.post_data = ""
 
-		self.payload_type = int(input("   > Payload type\n     >> 1-Numeric\n     >> 2-List\n     >> Insert ID:"))
-		if (self.payload_type == 1):
-			self.min_value = int(input("     >> Start value: "))
-			self.max_value = int(input("     >> End value: "))
-		
-		elif (self.payload_type == 2):
-			file = input("     >> File list: ")
-			opened_file = open(file,'r')
-			self.list = opened_file.readlines()
-		self.time = int(input("   > Delay between requests(seconds): "))
+		if (kwargs.get('delay') != None):
+			self.time = int(kwargs.get('delay'))
+		else:
+			self.time = 0
 
-	def present(count,request,method,payload):
 
-		print ("ID:{} METHOD:{} PAYLOAD: {} URL:[{}] CODE:{} SIZE:{}".format(count,method,payload,request.url,request.status_code,len(request.content)))
-		if (len(request.request.body) > 2):
-			print ("    BODY     {}".format(request.request.body))
-		if (request.content != None):
-			print ("    RESPONSE     {}".format(request.content))
+	def present(count,request,method,payload): #Present request info function
+		print ("| {} | {} | {} | {} | {} | {} |".format(count,payload,method.upper(),request.url,request.status_code,len(request.content)))
+
+	def prepare(self): #Request and validate params to explore
+
+		if (self.payload_type == 'num'): #If numeric payload type
+			self.min_value = int(input("     {}>>{} {}Start value:{} ".format(Fore.BLUE,Style.RESET_ALL,Fore.WHITE,Style.RESET_ALL)))
+			self.max_value = int(input("     {}>>{} {}End value:{} ".format(Fore.BLUE,Style.RESET_ALL,Fore.WHITE,Style.RESET_ALL)))
+			if (self.min_value >= self.max_value):
+				print ("|{}[ERROR]{} The start value is lower or equal the final value!".format(Fore.RED,Style.RESET_ALL))
+				return False
+			else:
+				self.requests_number = (self.max_value+1) -(self.min_value)
+				return True
+
+		if (self.payload_type == 'list'): #If list payload type
+			file = input("     {}>>{} {}File list:{} ".format(Fore.BLUE,Style.RESET_ALL,Fore.WHITE,Style.RESET_ALL))
+			try:
+				opened_file = open(file,'r')
+				self.list = opened_file.readlines()
+				self.requests_number = len(self.list)
+				return True
+			except:
+				print ("|{}[ERROR]{} Couldn't access the file!".format(Fore.RED,Style.RESET_ALL))
+				return False
 
 	def explore(self): #Start loop of requests
+		
+		#Present arguments to explore
+		print ("_"*100)
 
-		if (self.payload_type == 1): #Loop for numeric increase
+		print ("| {}METHOD{} {}".format(Fore.WHITE,Style.RESET_ALL,self.method.upper()))
+
+		print ("| {}URL{}  {}".format(Fore.WHITE,Style.RESET_ALL,self.url))
+
+		print ("| {}PAYLOAD{} {} | {} {} REQUESTS{}".format(Fore.WHITE,Style.RESET_ALL,
+															self.payload_type.upper(),
+															self.requests_number,
+															Fore.WHITE,Style.RESET_ALL))
+
+		print ("| {}TIME BETWEEN REQUESTS{} {} {}seconds".format(Fore.WHITE,Style.RESET_ALL,self.time,Fore.WHITE))
+		if (len(self.proxies)==2):
+			print ("| {}PROXY{} {}".format(Fore.WHITE,Style.RESET_ALL,self.proxies['http']))
+		input("       Press ENTER to start...")
+		print ("| ID | PAYLOAD | METHOD | URL | STATUS | SIZE |")
+
+		count = 0
+		if (self.payload_type == 'num'): #Loop for numeric increase
 			payload_number = self.min_value
-			count = 0
+			
 			for payload_number in range(self.min_value,self.max_value+1):
 				url = self.url.replace("IDOR",str(payload_number))
-				new_data = str(self.post_data)
-				defined_data = new_data.replace("IDOR",str(payload_number))
-				request = requests.request(self.method,url,headers=self.headers,cookies=self.cookies,
-							               proxies=self.proxies,verify=False,data=defined_data)
+				try:
+					new_data = str(self.post_data)
+					defined_data = new_data.replace("IDOR",str(payload_number))
+				except:
+					defined_data = ""
+				request = requests.request(self.method,url,headers=self.headers,
+										   proxies=self.proxies,verify=False,data=defined_data)
 				count += 1
 				target.present(count,request,self.method,payload_number)
 				time.sleep(self.time)
 
-		elif (self.payload_type == 2): #Loop for list file
+		elif (self.payload_type == 'list'): #Loop for list file
 			for element in self.list:
 				new_element = element.rstrip('\n')
 				url = self.url.replace("IDOR",new_element)
 				new_data = str(self.post_data)
 				defined_data = str(new_data.replace("IDOR",new_element))
-				request = requests.request(self.method,url,headers=self.headers,cookies=self.cookies,
-							               proxies=self.proxies,verify=False,data=defined_data)
+				request = requests.request(self.method,url,headers=self.headers,
+										   proxies=self.proxies,verify=False,data=defined_data)
+				count +=1
 				target.present(count,request,self.method,new_element)
 				time.sleep(self.time)
